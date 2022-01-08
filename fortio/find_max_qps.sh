6688c7e9a3a3a7d17868aa61e6a5be786ee4453b
ajoshi <asawari.joshi@infor.com>
2022-01-08 10:46:58 +0100

    tweaked the data gen script

---
+++ fortio/find_max_qps.sh
@@ -0,0 +1,12 @@
+
+qps=0
+connections=32
+time=3s
+url="http://a6a08666364e94722b0bff7a439c4d8b-7129ec495328f8fc.elb.eu-central-1.amazonaws.com/productpage"
+filename_prefix="test"
+
+for i in {1..30}
+do
+   kubectl exec fortio-d54c8d974-q8mc9 -n fortio -- fortio load -qps 0 -c 250 -t 30s -json ${filename_prefix}_${i}.json -a $url
+   sleep 5
+done
--- fortio/test_1.sh
+++
@@ -1,5 +0,0 @@
-for i in {3..30}
-do
-   kubectl exec fortio-d54c8d974-q8mc9 -n fortio -- fortio load -qps 0 -c 250 -t 30s -json test_$i.json -a http://a6a08666364e94722b0bff7a439c4d8b-7129ec495328f8fc.elb.eu-central-1.amazonaws.com/productpage
-   sleep 5
-done
