d9546dcc5f1d19e1f9094d0ed14a603ed6f42fdb
ajoshi <asawari.joshi@infor.com>
2022-01-09 10:57:39 +0100

    added data

--- istio/create.sh
+++ istio/create.sh
@@ -1,5 +1,14 @@
 #install istio 
 .\istioctl.exe install --set profile=default           
 
+
+#create application 
 kubectl apply -f bookinfo.yaml
 kubectl apply -f bookinfo-gateway.yaml
+
+#perform 1st test scenario where latency is checked for max QPS and 75% of the lowest qps identified 
+
+## later add monitoring stack when testing resource utilization
+cd monitoring
+kubectl apply -f .\grafana.yaml
+kubectl apply -f .\prometheus.yaml
